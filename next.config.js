6c031aaa4fb40b58f85ad505f22a6e1e70237491
Kedar G <kedar2712@gmail.com>
2019-04-27 22:53:25 -0400

    Updating script for heroku

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 // https://github.com/zeit/next-plugins/tree/master/packages/next-sass
 
+// const withSass = require('@zeit/next-sass')
+// console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
+// module.exports = process.env.NODE_ENV !== 'production' ?
+//   withSass() : {};
+
 const withSass = require('@zeit/next-sass')
-console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
-module.exports = process.env.NODE_ENV !== 'production' ?
-  withSass() : {};
+
+module.exports = () => {
+  return (process.env.NODE_ENV !== 'production')?
+    withSass() : {};
+}
